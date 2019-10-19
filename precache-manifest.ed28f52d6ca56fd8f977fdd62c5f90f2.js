76c677c41e6f833793035f95047b085af1309e0d
Somyanath Mohanty <mohantysomyanath@gmail.com>
2019-10-19 20:48:15 +0530

    Updates

---
+++ precache-manifest.ed28f52d6ca56fd8f977fdd62c5f90f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461b7b6981b5665d5be37597afa1bba7",
+    "url": "/form-validation/index.html"
+  },
+  {
+    "revision": "8cb66a87db86ef68386f",
+    "url": "/form-validation/static/css/main.0f440164.chunk.css"
+  },
+  {
+    "revision": "8a9a5f170cc841f5a5bc",
+    "url": "/form-validation/static/js/2.fbd49fb4.chunk.js"
+  },
+  {
+    "revision": "8cb66a87db86ef68386f",
+    "url": "/form-validation/static/js/main.29fdd083.chunk.js"
+  },
+  {
+    "revision": "cf4f52576e8e2ac8a92e",
+    "url": "/form-validation/static/js/runtime-main.3b2058f6.js"
+  }
+]);
